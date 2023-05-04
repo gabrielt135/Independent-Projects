6bf5c5debd74e17fe845c8711613c3af356de8a1
Gabriel Taveras <48261389+gabrielt135@users.noreply.github.com>
2023-05-04 15:53:59 -0400

    researching.py responses.py
    updated the near completed researching and responses files. (estimate 85% complete)

--- LT3R-BT/researching.py
+++ LT3R-BT/researching.py
@@ -1,15 +1,55 @@
-import requests as rq
+import fandom as fd
 from bs4 import BeautifulSoup
+import requests as rq
 
-def lookUp(weapon,  game)->str:
-    game.lower()
-    url = 'http://' + game+'.fandom.com/wiki/'+weapon
-    resp = rq.get(url)
-    
-    if resp.status_code == 403:
-        return "Unable to access page: Access Restricted."
-    elif resp.status_code == 404:
-        return "Unable to access page: Page does not exist."
-    elif resp.status_code == 200:
-        soup = BeautifulSoup(resp.text, 'html.parser')
+def getSum(url)->str:
+    retval =""
+    request = rq.get(url)
+    soup = BeautifulSoup(request.content, "html.parser")
+    pagePart = soup.find(class_= "mw-parser-output")
+    deleteList = pagePart.find_all(["table","dl", "aside", "h2", "ul"])
+    for i in deleteList:
+        i.decompose()
+    splitter = pagePart.get_text().lstrip("\n").split("\n")
+    retval = splitter[0]
+    return retval
+
+def research(weapon,  game,  set)->str:
+    page = ""
+    splits = game.split(' ')
+    parenthesis = "(" + game + ")"
+    if len(splits) > 1:
+        if splits[0].lower() == "the":
+            fd.set_wiki(splits[0].lower()+splits[1].lower())
+        else:
+            fd.set_wiki(splits[0].lower())
+        pages = fd.search(weapon, splits[0].lower())
+        for i in pages:
+            if i[0].find(parenthesis) > -1 and i[0].find(weapon) > -1:
+                page = fd.page(pageid = i[1])
+                break
+            else:
+                page = fd.page(pageid = pages[0][1])
+    else:
+        fd.set_wiki(game)
+        pages = fd.search(weapon,  game)
+        for i in pages:
+            if i[0].find(parenthesis) > -1 and i[0].find(weapon) > -1:
+                page = fd.page(pageid = i[1])
+                break
+            else:
+                page = fd.page(pageid = pages[0][1])
         
+    summary = getSum(page.url)
+        
+    if set == 1:
+        retstr = str(summary) +"\n\n" + str(page.section("Special Weapon Effects"))
+        retstr.replace('\'', '')
+        retstr.rstrip("\n")
+    elif set == 0:
+        retstr = str(summary) +"\n\n" + str(page.section("Special Weapon Effects")) + "\n\n" + str(page.section("Usage & Description")) + "\n\n" + str(page.section("Notes"))
+        retstr.replace('\'', '')
+        retstr.rstrip("\n")
+    else:
+        return "Invalid Setting, please use either \"Simple\" or \"Full\"."
+    return retstr
--- LT3R-BT/responses.py
+++ LT3R-BT/responses.py
@@ -1,14 +1,20 @@
-import random
+import researching
 
 def handle_response(message) -> str:
     p_message = message.lower()
-    if p_message == 'hello':
-        return 'Hey there!'
+    splitted = message.split("\n")
     
-    if p_message == 'roll':
-        return str(random.randint(1, 6))
+    set = None
+    if len(splitted) == 4:
+        if splitted[3].lower() == 'simple':
+            set = 1
+        elif splitted[3].lower() == 'full':
+            set = 0
+    
+    if splitted[0].lower() == '/search':
+        return researching.research(splitted[1],  splitted[2],  set)
         
     if p_message == '!help':
         return "`This is a help message that you can modify.`"
         
-    return "Invalid query. Try again."
+    return ""
